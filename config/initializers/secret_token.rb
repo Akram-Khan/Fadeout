18c11f99eff48d22faa43374c9d946537da24ebb
Akram Khan <akramkhan83@gmail.com>
2011-09-06 19:53:11 +0530

    fadeout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EXample::Application.config.secret_token = '24b5a8c1c920677a5f6465099092f733c51ec7da6e8e4895fdc4f90179ba1a509c4cfc5bb89957891778d5f5c83c0cdaa1e82cc7dae881cb3abf7012b72cc998'
